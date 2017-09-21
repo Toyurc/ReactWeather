e36988b28b0cb22dae4daa71d0d6fb0e414eb80d
toyosi.adebayo-ige <toyosi.adebayo-ige@konga.com>
2017-09-21 10:27:39 +0100

    prep for heroku

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express')
 
 //
 const app = express()
+const PORT = process.env.PORT || 5000
+app.use(function (req, res, next) {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next()
+  } else {
+    res.redirect('http://' + req.hostname + req.url)
+  }
+})
 app.use(express.static('public'))
 app.listen(5000, function () {
-  console.log('Express is listening at localhost:5000')
+  console.log('Express is listening at localhost:' + PORT)
 })
