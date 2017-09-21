a71daa2acce1d58d10677c704c70ca3ffac14d5f
toyosi.adebayo-ige <toyosi.adebayo-ige@konga.com>
2017-09-21 14:00:45 +0100

    about && example page

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -4,8 +4,18 @@ var about = (props) => {
   'use strict'
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page!</p>
+      <h1>About</h1>
+      <p>This is a weather built on React. I built it to learn about React</p>
+      <p>
+        Hear are some tools i used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript Library
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Maps</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
 
   )
